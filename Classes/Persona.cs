d6e962b045d5c4ea04dabab31f45b2e3d79f2bbd
Iago <iago.gc88@gmail.com>
2021-01-05 11:29:12 +0100

    E2345
    Exs 2345

---
+++ Classes/Persona.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET_T09 {
+    class Persona {
+        private string Nombre { get; set; }
+
+        public void SetNombre(string nombre) {
+            this.Nombre = nombre;
+        }
+
+        public void Saludar() {
+            Console.WriteLine("Hola soy {0}", this.Nombre);
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,27 @@ using System.Threading.Tasks;
 namespace NET_T09 {
     class Program {
         static void Main(string[] args) {
+            //Alumno Iago = new Alumno("Iago", 32);
+            //Iago.mostrarInfo();
+            //Iago.mayorEdad();
+
+            //Empleado Iago = new Empleado("Iago", 3500.25);
+            //Iago.mostrarInfo();
+            //Iago.pagarImpuestos();
+
+            //Operaciones o = new Operaciones(5, 8);
+            //o.Suma();
+            //o.Resta();
+            //o.Multiplicacion();
+            //o.Division();
+
+            //PruebaPersona prueba = new PruebaPersona();
+            //prueba.Main();
+
+            Libro Libro1 = new Libro();
+            Libro1.Main();
+
+            Console.ReadKey();
         }
     }
 }
